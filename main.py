8adacd55b1a7ac1939119e8c0ceb55b2be08b14e
Dalinar651 <65916948+Dalinar651@users.noreply.github.com>
2021-07-07 10:38:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 
 from bs4 import BeautifulSoup as bs
+import time
 import requests
 import pandas as pd
 
@@ -13,14 +14,24 @@ class shoeScraper:
         self.gender=""
         self.dataSet=list()
 
-    def getPageData(self,baseUrl,page):
-        params={"page":page}
+
+    def getPageData(self,baseUrl):
+        params={"page":"1"}
         url=baseUrl
         html=requests.get(url,params=params).text
         soup=bs(html,"html.parser")
-        master_list=soup.find_all("div",{"class":"bucket bucket-product-with-details bucket-product-with-attributes"})
-        for element in master_list:
-            self.getShoeInfo(element.select_one("a")["href"])
+        totalPages=soup.find("li",{"class":"last"}).text
+        totalPages=int(totalPages)
+        for i in range(totalPages+1):
+            params['page']=str(i+1)
+            print(params['page'])
+            html=requests.get(url,params=params).text
+            soup=bs(html,"html.parser")
+            master_list=soup.find_all("div",{"class":"bucket bucket-product-with-details bucket-product-with-attributes"})
+            for j,element in enumerate(master_list):
+                # time.sleep(0.5)
+                print(j)
+                self.getShoeInfo(element.select_one("a")["href"])
             
     def getShoeInfo(self,url):
         self.gender=url.split("/")[1]
@@ -50,7 +61,7 @@ class shoeScraper:
         
     def getCsvFile(self):
         df=pd.DataFrame(self.dataSet)
-        df.to_csv("Output_1.csv",index=True)
+        df.to_csv("Output_women_2.csv",index=True)
 
 
 # In[123]:
@@ -58,8 +69,7 @@ class shoeScraper:
 # provide the baseUrl and pageNo the scrape data from
 
 scraper=shoeScraper()
-# for i in range(10):
-scraper.getPageData("https://superbalist.com/browse/women/shoes/sneakers",3)
+scraper.getPageData("https://superbalist.com/browse/women/shoes/sneakers")
 
 
 # In[ ]:
